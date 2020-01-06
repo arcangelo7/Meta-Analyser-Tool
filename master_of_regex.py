9966496e735541c5ce6bdd03cc292732c2a6d009
sntcristian <snt.cristian@gmail.com>
2020-01-07 00:50:55 +0100

    new regex for authors initials and printed results

--- master_of_regex.py
+++ master_of_regex.py
@@ -1,3 +1,33 @@
+import csv
 import re
 
-master_of_regex = r'([^;\[]+)(?:\[(.*?)\])?;?'
+master_of_regex = r'([^;\[]+)(\[.*?\])?;?'
+authorsinitials = '([\w][^\s,]*)(?:[,\s]+([A-Z])[^\s\.\-]*(?:[\.\-\s]+([A-Z])[^\.\s\-]*)*)?'
+
+with open('metadata_sample.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = list(reader)
+
+authors = list()
+for line in data:
+    authors.append(line['author'])
+
+authors_list = list()
+for author in authors:
+    x = re.findall(r'([^;\[]+)(\[.*?\])?;?', author)
+    authors_list.extend(x)
+print(authors_list[0:10])
+
+authors_name = list()
+for authors_tuple in authors_list:
+    authors_name.append(authors_tuple[0])
+
+test_list = authors_name[0:100]
+authors_rep = list()
+for author in test_list:
+    rep = re.findall('([\w][^\s,]*)(?:[,\s]+([A-Z])[^\s\.\-]*(?:[\.\-\s]+([A-Z])[^\.\s\-]*)*)?', author)
+    authors_rep.append(' '.join(rep[0]))
+
+print(authors_rep)
+
+
